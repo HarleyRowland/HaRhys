741dd1376d84f498932ae2919bf891acfb49f354
Harley Rowland <hjr7@kent.ac.uk>
2016-03-22 11:19:24 +0000

    add worker

--- js/app.js
+++ js/app.js
@@ -222,6 +222,7 @@ function phoneNumberValidation(number){
 }
 
 function endState(text){
+    console.log("navigator", navigator);
     document.getElementById("app").innerHTML = "" +
     "<p style='margin-top: 50px;'>" + text + "</p>" +
     "<input type=\"checkbox\" checked data-toggle=\"toggle\" id=\"togglePlayCheckbox\" data-on=\"LookUp ON\" data-off=\"LookUp OFF\" onchange=\"togglePlay()\"> </input>" +
