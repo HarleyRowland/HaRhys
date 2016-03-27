885980c39a3042805d888a7b088b08b9fe428756
Harley Rowland <hjr7@kent.ac.uk>
2016-03-27 12:02:15 +0100

    Add web workers

--- js/worker.js
+++ js/worker.js
@@ -1,17 +1,3 @@
-var profile = {
-	userNum: null,
-	togglePlay: null,
-	locationLive: null,
-};
-
-onmessage = function(e) {
-  console.log("e.data[0].", e.data[0]);
-
-  if(e.data[0].userNum) profile.userNum = e.data[0].userNum;
-  if(e.data[0].togglePlay) profile.togglePlay = e.data[0].togglePlay;
-  if(e.data[0].locationLive) profile.locationLive = e.data[0].locationLive;
-}
-
 function timed(){
   setTimeout(function(){ 
     console.log("hello");
