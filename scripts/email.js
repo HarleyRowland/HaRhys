18c4800b3cbb8efa84e35f936543310cd091423a
Harley Rowland <harleyrowland17@gmail.com>
2016-09-30 14:38:57 +0100

    Small tidy up

--- scripts/email.js
+++ scripts/email.js
@@ -1,6 +1,6 @@
 $("textarea").keyup(function(){
   if($(this).val().length < 30){
-    $('.count').text("Characters left until can send: " + (30 - $(this).val().length));      
+    $('.count').text("Characters left until can you send: " + (30 - $(this).val().length));      
   } else {
     $('.count').text("Characters left: " + (500 - $(this).val().length));      
   }
