6e503257e2d65bfe83ef977da620cbdd2ad3950d
Harley Rowland <harleyrowland17@gmail.com>
2016-10-04 00:22:18 +0100

    Change nav-bar top-bar

--- scripts/nav-bar.js
+++ scripts/nav-bar.js
@@ -1,4 +1,3 @@
-$( document ).ready(function() {
   $( ".more-info" ).click(function() {
     $('html, body').animate({
       scrollTop: $(".about-me").offset().top
@@ -18,6 +17,7 @@ $( document ).ready(function() {
   });
 
   $( "#nav-portfolio" ).click(function() {
+    console.log('hello');
     $('html, body').animate({
       scrollTop: $(".portfolio").offset().top
     }, 1500);
@@ -29,31 +29,72 @@ $( document ).ready(function() {
     }, 1500);
   });
 
+  $( "#menu-top" ).click(function() {
+    $('html, body').animate({
+      scrollTop: $("body").offset().top
+    }, 1500);
+    $('.drop-down').hide();
+  });
+
+  $( "#menu-about" ).click(function() {
+    $('html, body').animate({
+      scrollTop: $(".about-me").offset().top
+    }, 1500);
+    $('.drop-down').hide();
+  });
+
+  $( "#menu-contact" ).click(function() {
+    $('html, body').animate({
+      scrollTop: $(".contact-me").offset().top
+    }, 1500);
+    $('.drop-down').hide();
+  });
+
+  $( "#menu-portfolio" ).click(function() {
+    $('html, body').animate({
+      scrollTop: $(".portfolio").offset().top
+    }, 1500);
+    $('.drop-down').hide();
+  });
+
+  $( ".menu" ).click(function(e) {
+    e.preventDefault();
+    if($(".drop-down").visible()){
+      $('.drop-down').hide();
+    } else {
+      $('.drop-down').show();      
+    }
+  });
+
+$(window).resize(function() {
+  if(window.innerWidth > 640){
+    $('.drop-down').hide();
+  }
 });
 
 $(window).scroll(function(){
   if($(".landing .heading").visible()){
-    $("#nav-landing").addClass("active");
-    $("#nav-about").removeClass("active");
-    $("#nav-contact").removeClass("active");
-    $("#nav-portfolio").removeClass("active");
+    $("#nav-landing, #menu-top").addClass("active");
+    $("#nav-about, #menu-about").removeClass("active");
+    $("#nav-contact, #menu-contact").removeClass("active");
+    $("#nav-portfolio, #menu-portfolio").removeClass("active");
   }
   if($(".bio-text").visible()){
-    $("#nav-about").addClass("active");
-    $("#nav-landing").removeClass("active");
-    $("#nav-contact").removeClass("active");
-    $("#nav-portfolio").removeClass("active");
+    $("#nav-about, #menu-about").addClass("active");
+    $("#nav-landing, #menu-top").removeClass("active");
+    $("#nav-contact, #menu-contact").removeClass("active");
+    $("#nav-portfolio, #menu-portfolio").removeClass("active");
   }
   if($(".email-form").visible()){
-    $("#nav-contact").addClass("active");
-    $("#nav-landing").removeClass("active");
-    $("#nav-about").removeClass("active");
-    $("#nav-portfolio").removeClass("active");
+    $("#nav-contact, #menu-contact").addClass("active");
+    $("#nav-landing, #menu-top").removeClass("active");
+    $("#nav-about, #menu-about").removeClass("active");
+    $("#nav-portfolio, #menu-portfolio").removeClass("active");
   }
   if($(".webLink").visible()){
-    $("#nav-portfolio").addClass("active");
-    $("#nav-landing").removeClass("active");
-    $("#nav-about").removeClass("active");
-    $("#nav-contact").removeClass("active");
+    $("#nav-portfolio, #menu-portfolio").addClass("active");
+    $("#nav-landing, #menu-top").removeClass("active");
+    $("#nav-about, #menu-about").removeClass("active");
+    $("#nav-contact, #menu-contact").removeClass("active");
   }
 }); 
