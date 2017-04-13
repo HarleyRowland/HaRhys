6f8406e1c30c8d2c9f77afae940686b79e61d34a
Harley Rowland <harleyrowland17@gmail.com>
2017-04-13 22:23:49 +0100

    Implement email

--- kentcounty/scripts/email.js
+++ kentcounty/scripts/email.js
@@ -1,4 +1,6 @@
+
 $( "#submit-button" ).click(function(){
+  console.log("hello")
   sendMail();
 });
 
@@ -6,7 +8,9 @@ $( ".clear" ).click(function(){
   $('#name').val('');
   $('#email').val('');
   $('#email-body').val('');
-  $('.count').text("Characters left until you can send: 30");
+  $('#number').val('');
+  $('#date').val('');
+  $('#package').val('');
 });
 
 function sendMail() {
@@ -15,13 +19,15 @@ function sendMail() {
   var name = escape(document.getElementById('name').value);
   var email = escape(document.getElementById('email').value);
   var number = escape(document.getElementById('number').value);
+  var package = escape(document.getElementById('package').value);
   var date = escape(document.getElementById('date').value);
   var body = escape(document.getElementById('email-body').value);
-  console.log(date)
 
-  var url = "https://arcane-anchorage-33274.herokuapp.com/kcEmail?name="+name+"&email="+email+"&number="+number+"&date="+date+"&body="+body;
+  var url = "https://arcane-anchorage-33274.herokuapp.com/kcEmail?name="+name+"&email="+email+"&number="+number+"&package="+package+"&date="+date+"&body="+body;
 
   var canSend = validateForm(name, date, number, email); 
+
+  $('#submit-button').removeAttr('disabled'); 
 
   callAPI(canSend, url, body);
 }
@@ -48,6 +54,7 @@ var validateForm = function(name, date, number, email){
     canSend = false;
     $('#email').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
       $('#email').removeClass('shake');
+      $('#email').removeClass('animated');
     });
   }
   if(date == ""){
@@ -66,7 +73,7 @@ var callAPI = function(canSend, url, body){
     $.ajax({
       url: url, 
       success: function(result){
-        $('.messageSent').text('You have sent to following message to Harley Rowland: ' + unescape(body));
+        $('.messageSent').text('We aim to reply within 48 hours. We will be in touch soon.');
         $('.modal').trigger('click');
         $('.email-button').removeAttr('disabled');
         $('.message-send-overlay').hide();
@@ -83,7 +90,20 @@ var callAPI = function(canSend, url, body){
   }
 }
 
+var getPackage = function() {
+  var url = location.href;
+  if(url.includes("bronze")){
+    $('#package').val('Bronze Package');
+  } else if(url.includes("silver")){
+    $('#package').val('Silver Package');
+  } else if(url.includes("gold")){
+    $('#package').val('Gold Package');
+  } else if(url.includes("platinum")){
+    $('#package').val('Platinum Package');
+  }
+}
 
+getPackage()
 
 var validateEmail = function(email) {
   var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
