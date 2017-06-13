2f1c7911d0e1b24555ba1a1f48a227b85f4d2ae6
Harley Rowland <harleyrowland@harleys-mbp.home>
2017-06-13 21:57:49 +0100

    Fix error

--- cv/js/contact.js
+++ cv/js/contact.js
@@ -1,17 +1,20 @@
+initAPI()
+
 $("#email-body").keyup(function(){
+  console.log("hi")
   if($("#email-body").val().length < 30){
-    $('#count').text("Characters until send: " + (30 - $("#email-body").val().length));      
+    $('.count').text("Characters until send: " + (30 - $("#email-body").val().length));      
   } else {
-    $('#count').text("Characters left: " + (500 - $("#email-body").val().length));      
+    $('.count').text("Characters left: " + (500 - $("#email-body").val().length));      
   }
 });
 
-$("#submit-button").click(function(){
+$(".submit-button").click(function(){
   sendMail()
 });
 
 function sendMail() {
-  // $('.email-button').attr('disabled','disabled');
+  $('.email-button').attr('disabled','disabled');
 
   var name = escape(document.getElementById('name').value);
   var number = escape(document.getElementById('number').value);
@@ -22,8 +25,40 @@ function sendMail() {
   var url = "https://arcane-anchorage-33274.herokuapp.com/email?name="+name+"&subject="+subject+"&email="+email+"&number="+number+"&body="+body;
 
   var canSend = validateForm(name, email, subject, body); 
-  console.log("MESSAGE SENT!")
-  // callAPI(canSend, url, body);
+  callAPI(canSend, url, body);
+}
+
+var initAPI = function(){
+  $.ajax({
+      url: "https://arcane-anchorage-33274.herokuapp.com", 
+      success: function(result){
+        $('.email-button').removeAttr('disabled');
+      }, 
+      error: function (xhr, ajaxOptions, thrownError) {
+        console.log(xhr.status, thrownError);
+        alert("There has been an error sending this message. Please try again later!")
+        $('.email-button').removeAttr('disabled');
+      }
+    });
+}
+
+var callAPI = function(canSend, url, body){
+  if(canSend){
+    $.ajax({
+      url: url, 
+      success: function(result){
+        console.log("MESSAGE SENT!")
+        $('.email-button').removeAttr('disabled');
+      }, 
+      error: function (xhr, ajaxOptions, thrownError) {
+        console.log(xhr.status, thrownError);
+        alert("There has been an error sending this message. Please try again later!")
+        $('.email-button').removeAttr('disabled');
+      }
+    });
+  } else {
+    $('#submit-button').removeAttr('disabled'); 
+  }
 }
 
 var validateForm = function(name, email, subject, body){
