3e31e4b6acd337438151ee03f33b9bf28a8ee22a
Harley Rowland <harleyrowland17@gmail.com>
2017-06-25 21:30:51 +0100

    Add pbk

---
+++ pbk/js/landing.js
@@ -0,0 +1,22 @@
+setTimeout(bounceArrow, 4000);
+function bounceArrow() {
+	$('#landing .bouncingArrow').addClass('bounce');
+	$('#landing .bouncingArrow').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+      $('#landing #bouncingArrow').removeClass('bounce');
+    });
+  	setTimeout(bounceArrow, 4000);
+}
+
+$( ".bouncingArrow" ).click(function() {
+	$('html, body').animate({
+  		scrollTop: $("#bio").offset().top-50
+	}, 1500);
+});
+
+$( ".clear" ).click(function(){
+  $('#name').val('');
+  $('#subject').val('');
+  $('#email').val('');
+  $('#email-body').val('');
+  $('.count').text("Characters until send: 30");
+});
