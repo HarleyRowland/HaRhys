b172bc8d47d26e2f76d6aee835f097e691f06420
Harley Rowland <harleyrowland17@gmail.com>
2017-10-05 07:40:28 +0100

    Add changes for first draft

--- pbk/js/landing.js
+++ pbk/js/landing.js
@@ -2,21 +2,14 @@ setTimeout(bounceArrow, 4000);
 function bounceArrow() {
 	$('#landing .bouncingArrow').addClass('bounce');
 	$('#landing .bouncingArrow').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-      $('#landing #bouncingArrow').removeClass('bounce');
+      $('#landing .bouncingArrow').removeClass('bounce');
     });
   	setTimeout(bounceArrow, 4000);
 }
 
-$( ".bouncingArrow" ).click(function() {
-	$('html, body').animate({
-  		scrollTop: $("#bio").offset().top-50
-	}, 1500);
-});
 
-$( ".clear" ).click(function(){
-  $('#name').val('');
-  $('#subject').val('');
-  $('#email').val('');
-  $('#email-body').val('');
-  $('.count').text("Characters until send: 30");
+$( ".bouncingArrow" ).click(function() {
+  $('html, body').animate({
+      scrollTop: $("#packages").offset().top-50
+  }, 1500);
 });
